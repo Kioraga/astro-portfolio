e1a4b0dbdd317a610b66b73d5a23680e6669c4cd
Daniel Cancelas <dcancelasgarcia@gmail.com>
2024-11-25 18:43:10 +0100

    Adding .gitignore files and configuring TinaCMS

---
+++ tina/config.ts
@@ -0,0 +1,100 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch =
+  process.env.GITHUB_BRANCH ||
+  process.env.VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  "main";
+
+export default defineConfig({
+  branch,
+
+  // Get this from tina.io
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  // Get this from tina.io
+  token: process.env.TINA_TOKEN,
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public",
+    },
+  },
+  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
+  schema: {
+    collections: [
+      {
+        name: 'post',
+        label: 'Posts',
+        path: 'src/content/post',
+        format: 'md',
+        fields: [
+          {
+            type: 'string',
+            name: 'title',
+            label: 'Title',
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: 'string',
+            name: 'description',
+            label: 'Description',
+            required: true,
+          },
+          {
+            type: 'datetime',
+            name: 'publishDate',
+            label: 'Publish Date',
+            required: true,
+          },
+          {
+            type: 'datetime',
+            name: 'updatedDate',
+            label: 'Updated Date',
+            required: false,
+          },
+          {
+            type: 'object',
+            name: 'coverImage',
+            label: 'Cover Image',
+            fields: [
+              {
+                type: 'image',
+                name: 'src',
+                label: 'Image Source',
+              },
+              {
+                type: 'string',
+                name: 'alt',
+                label: 'Alt Text',
+              },
+            ],
+          },
+          {
+            type: 'boolean',
+            name: 'draft',
+            label: 'Draft',
+          },
+          {
+            type: 'string',
+            list: true,
+            name: 'tags',
+            label: 'Tags',
+          },
+          {
+            type: 'rich-text',
+            name: 'body',
+            label: 'Content',
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
